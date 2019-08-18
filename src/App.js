0815e97639f844b726deaac8f567b0f4a0aece29
Jungbum Woo <woojungbum12@gmail.com>
2019-08-18 23:28:45 +0900

    Let's gigi

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ class App extends React.Component {
     count: 0
   };
   add = () => {
-    console.log("add");
+    this.setState(current => ({ count: current.count + 1 }));
   };
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }));
+  };
+  componentDidMount() {
+    console.log("component renderd");
+  };
+  componentDidUpdate() {
+    console.log("I just updated!!")
   };
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
