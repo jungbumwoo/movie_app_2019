2d536bb358bdbe861151ccb87b9e284696059c8f
Jungbum Woo <woojungbum12@gmail.com>
2019-08-23 08:17:22 +0900

    0823

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import { directive, throwStatement } from '@babel/types';
 
 class App extends React.Component {
   state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true
   };
   componentDidMount() {
-    console.log("component renderd");
-  };
-  componentDidUpdate() {
-    console.log("I just updated!!")
-  };
-  render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000)
   }
-}
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+  }
+};
 
 export default App;
