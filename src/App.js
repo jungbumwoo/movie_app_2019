4a47cba1a088465ca06566a2f34091478e25513e
Jungbum Woo <woojungbum12@gmail.com>
2019-08-23 12:10:37 +0900

    0823_12:10

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import React from 'react';
-import PropTypes from "prop-types";
-import { directive, throwStatement } from '@babel/types';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    movies: []
   };
+  getMovies = async () => {
+    const {data: {  data :{movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    console.log(movies);
+    this.setState({movies:movies, isLoading: false}) //({movies})
+  };  
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000)
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    const { isLoading, movies } = this.state;
+    return (<div>{isLoading ? "Loading..." : movies.map(movie => {
+      console.log(movie);
+      return (<Moive key={movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>);
+    })}</div>)
   }
 };
 
 export default App;
+
+
