783c1dde0879ea882a8cf92bfcc19b3582d84082
Jungbum Woo <woojungbum12@gmail.com>
2019-08-26 09:10:21 +0900

    Updates

---
+++ precache-manifest.ad2d4043af27d3d299cdc76866935cc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020e502f6b0943c99913d6195092a926",
+    "url": "/github.io/jungbumwoo/movie_app_2019/index.html"
+  },
+  {
+    "revision": "fe0e3eae737bfe950c7c",
+    "url": "/github.io/jungbumwoo/movie_app_2019/static/css/main.b5795f18.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/github.io/jungbumwoo/movie_app_2019/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "fe0e3eae737bfe950c7c",
+    "url": "/github.io/jungbumwoo/movie_app_2019/static/js/main.f8fe66cd.chunk.js"
+  },
+  {
+    "revision": "d770cdcbe0e8699ace19",
+    "url": "/github.io/jungbumwoo/movie_app_2019/static/js/runtime~main.7eefcad8.js"
+  }
+]);
